91a28140b53ede0c67b2cfffef6b5a091e1fa653
José A. Q. C. Gomes <jose.augusto.comparotto@gmail.com>
2025-01-22 17:22:11 -0300

    fix: Substituição do pacote 'pyodbc' pelo 'pymssql'
    Suprime a necessidade do Driver ODBC que não está instalado no ambiente de produção.

--- function_app.py
+++ function_app.py
@@ -1,32 +1,25 @@
 import azure.functions as func
 import logging
-from pyodbc import Cursor, Row
-import pyodbc
+import pymssql
 import json
 import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # Configuração da conexão com o banco de dados SQL Server
-def get_db_connection() -> pyodbc.Connection:
+def get_db_connection() -> pymssql.Connection:
     server = os.getenv("SQL_SERVER")
     database = os.getenv("SQL_DATABASE")
     username = os.getenv("SQL_USERNAME")
     password = os.getenv("SQL_PASSWORD")
 
-    driver = "{ODBC Driver 18 for SQL Server}"
-
-    conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-
-    print(conn_str)
-
-    return pyodbc.connect(conn_str)
+    return pymssql.connect(server, username, password, database)
 
 @app.route(route="produtos", methods=['get'])
 def getAllProducts(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        cursor: Cursor = get_db_connection().cursor()
+        cursor: pymssql.Cursor = get_db_connection().cursor()
 
         cursor.execute("SELECT Id, Nome, Descricao, Preco, QuantidadeEstoque, Categoria FROM [dbo].[Produtos]")
 
