4ae933e6c3e2b490f718a0930c138de163b1d717
José A. Q. C. Gomes <jose.augusto.comparotto@gmail.com>
2025-01-22 16:17:30 -0300

    Adicionada a função 'get_db_connection' para conexão com o Banco de Dados

--- function_app.py
+++ function_app.py
@@ -6,3 +6,19 @@ import json
 import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+# Configuração da conexão com o banco de dados SQL Server
+def get_db_connection() -> pyodbc.Connection:
+    server = os.getenv("SQL_SERVER")
+    database = os.getenv("SQL_DATABASE")
+    username = os.getenv("SQL_USERNAME")
+    password = os.getenv("SQL_PASSWORD")
+
+    driver = "{ODBC Driver 18 for SQL Server}"
+
+    conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
+    print(conn_str)
+
+    return pyodbc.connect(conn_str)
+ 
