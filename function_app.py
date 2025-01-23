5140f2419f1352ee0d9ce36cf801cb3cf54ca1b6
José A. Q. C. Gomes <jose.augusto.comparotto@gmail.com>
2025-01-23 11:07:34 -0300

    Adicionada a rota 'getAllProducts': GET /produtos/{id}

--- function_app.py
+++ function_app.py
@@ -3,6 +3,7 @@ import logging
 import pymssql
 import json
 import os
+import re
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -35,6 +36,49 @@ def getAllProducts(req: func.HttpRequest) -> func.HttpResponse:
         ]
 
         return func.HttpResponse(json.dumps(produtos), mimetype="application/json", status_code=200)
+
+    except Exception as e:
+        logging.error(f"Database connection failed: {e}")
+        return func.HttpResponse("Error fetching products data.", status_code=500)
+
+@app.route(route="produtos/{id}", methods=['get'])
+def getProductsById(req: func.HttpRequest) -> func.HttpResponse:
+
+    product_id = req.route_params["id"]
+
+    if not re.match(r'^\d+$', product_id):
+        return func.HttpResponse("Malformed parameter. Expected integer Id.", mimetype="text/plain", status_code=400)
+
+    try:
+        cursor: pymssql.Cursor = get_db_connection().cursor()
+        
+        cursor.execute("""
+            SELECT 
+                Id,
+                Nome,
+                Descricao,
+                Preco,
+                QuantidadeEstoque,
+                Categoria
+            FROM [dbo].[Produtos]
+            WHERE Id = %d
+        """, product_id)
+
+        row = cursor.fetchone()
+
+        if row == None:
+            return func.HttpResponse("Resource not found in database.", mimetype="text/plain", status_code=404)
+
+        produto = {
+            "id":   row[0],
+            "nome": row[1],
+            "descircao": row[2],
+            "preco": float(row[3]),
+            "quantidade_estoque": row[4],
+            "categoria": row[5],
+        }
+
+        return func.HttpResponse(json.dumps(produto), mimetype="application/json", status_code=200)
 
     except Exception as e:
         logging.error(f"Database connection failed: {e}")
